1b6fc432a4dee22988cba752e5a4172557e79bad
Алексей Акулич <61252310+loki87by@users.noreply.github.com>
2021-03-01 14:48:24 +0300

    feat: write sketch

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -3,7 +3,8 @@ const CustomError = require("../extensions/custom-error");
 const MODERN_ACTIVITY= 15; 
 const HALF_LIFE_PERIOD= 5730;
 
-module.exports = function dateSample(/* sampleActivity */) {
+module.exports = function dateSample(sampleActivity) {
   throw new CustomError('Not implemented');
   // remove line with error and write your code here
+  Math.log(MODERN_ACTIVITY / sampleActivity) / (Math.sqrt(2)/HALF_LIFE_PERIOD)
 };
