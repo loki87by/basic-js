3275f6e4bc6bfdc9329de0837852dec06c0ace15
Алексей Акулич <61252310+loki87by@users.noreply.github.com>
2021-03-01 15:30:57 +0300

    feat: write code sketch

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -1,7 +1,19 @@
 const CustomError = require("../extensions/custom-error");
 
 module.exports = function repeater(/* str, options */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+  //throw new CustomError('Not implemented');
+  let additions = [str]
+  for(let i=0; i<options.additionRepeatTimes; i++){
+    additions.push(options.addition)
+    additions.push(options.additionSeparator)
+  }
+  additions.pop()
+  additions.push(options.separator)
+  let res = []
+  for(let j=0; j<options.repeatTimes; j++){
+    res.push(...additions)
+  }
+  res.pop()
+  return res.join('')
 };
   
