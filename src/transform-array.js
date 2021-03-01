a5ccc982f1c460b3f69fedd52c47a6652d0a628d
Алексей Акулич <61252310+loki87by@users.noreply.github.com>
2021-03-01 18:55:03 +0300

    feat: write function sketch

--- src/transform-array.js
+++ src/transform-array.js
@@ -1,6 +1,16 @@
 const CustomError = require("../extensions/custom-error");
 
 module.exports = function transform(/* arr */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+  //throw new CustomError('Not implemented');
+  let res = []
+  for(let j=0; j< arr.length; j++){
+    if(typeof arr[j] !== 'string'){res.push(arr[j])}
+    else {
+      if(arr[j] === '--discard-next'){j ++}
+      else if(arr[j] === '--discard-prev'){res.pop()}
+      else if(arr[j] === '--double-next'){res.push(arr[j+1])}
+      else if(arr[j] === '--double-prev'){res.push(arr[j-1])}
+    }
+  }
+  return res
 };
