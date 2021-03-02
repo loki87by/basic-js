ec0230666d8d22f6d8c84bc8991fdbd795e74e17
Алексей Акулич <61252310+loki87by@users.noreply.github.com>
2021-03-02 12:06:20 +0300

    feat: write sketch of seasons

--- src/what-season.js
+++ src/what-season.js
@@ -1,6 +1,13 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function getSeason(/* date */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function getSeason(date) {
+  if(!isNan(Date.parse(date))) {
+    throw new CustomError('Not implemented');}
+  if((date === "undefined") || (date === "null")){
+    return 'Unable to determine the time of year!'}
+  let month = date.getMonths();
+  if((month > 2) && (month < 6)){return 'spring'}
+  else if((month > 5) && (month < 9)){return 'summer'}
+  else if((month > 8) && (month < 12)){return 'autumn'}
+  else{return 'winter'}
 };
